bb7981500295542ddfe336b6e93b03ccdabcd7da
SatellightsJimin <plaguefor@gmail.com>
2014-08-14 18:33:52 +0900

    커밋

--- IDCollector/IDCollectorWindow.Designer.cs
+++ IDCollector/IDCollectorWindow.Designer.cs
@@ -28,128 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IDCollectorWindow));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.folderLbl = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
-            this.testerBtn = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.allCases = new System.Windows.Forms.Button();
+            this.resultListView = new System.Windows.Forms.ListView();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.deleteFile = new System.Windows.Forms.Button();
+            this.onlyHyphen = new System.Windows.Forms.Button();
+            this.mainGridView = new System.Windows.Forms.DataGridView();
+            this.checkFile = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.filePathColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.foundIdColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.folderOpenButtonColumn = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.fileOpenColumn = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.deleteFileColumn = new System.Windows.Forms.DataGridViewButtonColumn();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
             this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.listView1, 0, 1);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.resultListView, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.mainGridView, 0, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.709957F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 93.29005F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(711, 462);
-            this.tableLayoutPanel1.TabIndex = 0;
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.folderLbl);
             this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.testerBtn);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Controls.Add(this.allCases);
+            resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(711, 31);
-            this.panel1.TabIndex = 1;
             // 
-            // label1
+            // folderLbl
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(185, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 12);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            resources.ApplyResources(this.folderLbl, "folderLbl");
+            this.folderLbl.Name = "folderLbl";
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(85, 4);
+            resources.ApplyResources(this.button4, "button4");
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 2;
-            this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // testerBtn
+            // allCases
             // 
-            this.testerBtn.Location = new System.Drawing.Point(4, 4);
-            this.testerBtn.Name = "testerBtn";
-            this.testerBtn.Size = new System.Drawing.Size(75, 23);
-            this.testerBtn.TabIndex = 1;
-            this.testerBtn.Text = "Tester";
-            this.testerBtn.UseVisualStyleBackColor = true;
-            this.testerBtn.Click += new System.EventHandler(this.testerBtn_Click);
+            resources.ApplyResources(this.allCases, "allCases");
+            this.allCases.Name = "allCases";
+            this.allCases.UseVisualStyleBackColor = true;
+            this.allCases.Click += new System.EventHandler(this.testerBtn_Click);
             // 
-            // button3
+            // resultListView
             // 
-            this.button3.Location = new System.Drawing.Point(629, 3);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 0;
-            this.button3.Text = "\'-\' 포함";
-            this.button3.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.resultListView, "resultListView");
+            this.resultListView.Name = "resultListView";
+            this.resultListView.UseCompatibleStateImageBehavior = false;
+            this.resultListView.View = System.Windows.Forms.View.Details;
+            this.resultListView.SelectedIndexChanged += new System.EventHandler(this.resultListView_SelectedIndexChanged);
+            this.resultListView.DoubleClick += new System.EventHandler(this.OpenExplorer);
             // 
-            // button2
+            // panel2
             // 
-            this.button2.Location = new System.Drawing.Point(548, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "\'-\' 제외";
-            this.button2.UseVisualStyleBackColor = true;
+            this.panel2.Controls.Add(this.deleteFile);
+            this.panel2.Controls.Add(this.onlyHyphen);
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
             // 
-            // button1
+            // deleteFile
             // 
-            this.button1.Location = new System.Drawing.Point(467, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "모두찾기";
-            this.button1.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.deleteFile, "deleteFile");
+            this.deleteFile.Name = "deleteFile";
+            this.deleteFile.UseVisualStyleBackColor = true;
             // 
-            // listView1
+            // onlyHyphen
             // 
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.Location = new System.Drawing.Point(3, 34);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(705, 425);
-            this.listView1.TabIndex = 2;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.List;
+            resources.ApplyResources(this.onlyHyphen, "onlyHyphen");
+            this.onlyHyphen.Name = "onlyHyphen";
+            this.onlyHyphen.UseVisualStyleBackColor = true;
+            // 
+            // mainGridView
+            // 
+            this.mainGridView.AllowUserToDeleteRows = false;
+            this.mainGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.mainGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mainGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.checkFile,
+            this.filePathColumn,
+            this.foundIdColumn,
+            this.folderOpenButtonColumn,
+            this.fileOpenColumn,
+            this.deleteFileColumn});
+            resources.ApplyResources(this.mainGridView, "mainGridView");
+            this.mainGridView.Name = "mainGridView";
+            this.mainGridView.RowHeadersVisible = false;
+            this.mainGridView.RowTemplate.Height = 23;
+            this.mainGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // checkFile
+            // 
+            resources.ApplyResources(this.checkFile, "checkFile");
+            this.checkFile.Name = "checkFile";
+            // 
+            // filePathColumn
+            // 
+            resources.ApplyResources(this.filePathColumn, "filePathColumn");
+            this.filePathColumn.Name = "filePathColumn";
+            this.filePathColumn.ReadOnly = true;
+            // 
+            // foundIdColumn
+            // 
+            resources.ApplyResources(this.foundIdColumn, "foundIdColumn");
+            this.foundIdColumn.Name = "foundIdColumn";
+            this.foundIdColumn.ReadOnly = true;
+            // 
+            // folderOpenButtonColumn
+            // 
+            resources.ApplyResources(this.folderOpenButtonColumn, "folderOpenButtonColumn");
+            this.folderOpenButtonColumn.Name = "folderOpenButtonColumn";
+            this.folderOpenButtonColumn.Text = "폴더열기";
+            this.folderOpenButtonColumn.UseColumnTextForButtonValue = true;
+            // 
+            // fileOpenColumn
+            // 
+            resources.ApplyResources(this.fileOpenColumn, "fileOpenColumn");
+            this.fileOpenColumn.Name = "fileOpenColumn";
+            this.fileOpenColumn.Text = "파일열기";
+            this.fileOpenColumn.UseColumnTextForButtonValue = true;
+            // 
+            // deleteFileColumn
+            // 
+            resources.ApplyResources(this.deleteFileColumn, "deleteFileColumn");
+            this.deleteFileColumn.Name = "deleteFileColumn";
+            this.deleteFileColumn.Text = "삭제";
+            this.deleteFileColumn.UseColumnTextForButtonValue = true;
             // 
             // IDCollectorWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 462);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "IDCollectorWindow";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.IDCollectorWindow_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mainGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -158,14 +192,21 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button testerBtn;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button allCases;
+        private System.Windows.Forms.ListView resultListView;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label folderLbl;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button deleteFile;
+        private System.Windows.Forms.Button onlyHyphen;
+        private System.Windows.Forms.DataGridView mainGridView;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn checkFile;
+        private System.Windows.Forms.DataGridViewTextBoxColumn filePathColumn;
+        private System.Windows.Forms.DataGridViewComboBoxColumn foundIdColumn;
+        private System.Windows.Forms.DataGridViewButtonColumn folderOpenButtonColumn;
+        private System.Windows.Forms.DataGridViewButtonColumn fileOpenColumn;
+        private System.Windows.Forms.DataGridViewButtonColumn deleteFileColumn;
 
     }
 }
--- IDCollector/IDCollectorWindow.cs
+++ IDCollector/IDCollectorWindow.cs
@@ -2,75 +2,195 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+using Spire.Doc;
+using Spire.Xls;
+
 
 namespace IDCollector
 {
     public partial class IDCollectorWindow : Form
     {
-
+        StringBuilder IdStrings;
         string path = @"c:\";
+
+
+        //정규식
+        //1. 하이픈 있음
+        string patternHyphen = "[0-9]{2}(0[1-9]|1[012])(0[1-9]|1[0-9]|2[0-9]|3[01])-[012349][0-9]{6}";
+        //2 하이픈 없음
+        string patternNoHyphen = "[0-9]{2}(0[1-9]|1[012])(0[1-9]|1[0-9]|2[0-9]|3[01])[012349][0-9]{6}";
+
         public delegate void _AddList(string f);
+
         public IDCollectorWindow()
         {
             InitializeComponent();
-            label1.Text = @"c:\";
-      
+            folderLbl.Text = @"c:\";
+            ColumnHeader pathHeader = new ColumnHeader();   // 헤더 생성
+            pathHeader.Text = "파일주소";     // 헤더에 들어갈 텍스트
+            pathHeader.Width = 300;
+            pathHeader.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
+
+            resultListView.Columns.Add(pathHeader);  //  ListView에 헤더컬럼 추가
+
+            ColumnHeader idHeader = new ColumnHeader();   // 헤더 생성
+            idHeader.Text = "주민등록번호";     // 헤더에 들어갈 텍스트
+            idHeader.Width = 300;
+            idHeader.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
+            resultListView.Columns.Add(idHeader);  //  ListView에 헤더컬럼 추가
+
+            mainGridView.Columns[2].FillWeight = 200;
+            mainGridView.Columns[1].FillWeight = 400;
+            mainGridView.Columns[0].FillWeight = 50;
+
+
+
         }
 
         private void testerBtn_Click(object sender, EventArgs e)
         {
-            
-            //Thread t1 = new Thread(new ParameterizedThreadStart(AddList));
-            //t1.Start(@"d:\");
 
-            listView1.Items.Clear();
+            resultListView.Items.Clear();
 
-            testerBtn.Enabled = false;
+
             button4.Enabled = false;
-            label1.Text= "Searching...";
+            folderLbl.Text = "Searching...";
             this.Cursor = Cursors.WaitCursor;
 
-            Application.DoEvents();
+            System.Windows.Forms.Application.DoEvents();
 
-            FindFiles(path);
+            FindFiles(path, (Button)sender);
 
-            label1.Text = "Search";
+            MessageBox.Show("검색이 완료되었습니다");
+            folderLbl.Text = path;
             this.Cursor = Cursors.Default;
-            
-            testerBtn.Enabled = true;
+
             button4.Enabled = true;
         }
 
-        private void FindFiles(string sDir)
+        private void FindFiles(string sDir, Button sender)
         {
+            string fileContents = null; //문서가 가지고 있는 내용
             try
             {
-                foreach (string f in Directory.GetFiles(sDir, "*.txt"))
+                foreach (string filePath in Directory.GetFileSystemEntries(sDir, "*.*").Where(file => System.IO.Path.GetExtension(file).ToLower().Equals(".txt") || System.IO.Path.GetExtension(file).ToLower().Equals(".doc")
+                        || System.IO.Path.GetExtension(file).ToLower().Equals(".docx") || System.IO.Path.GetExtension(file).ToLower().Equals(".xls")
+                        || System.IO.Path.GetExtension(file).ToLower().Equals(".xlsx")))
                 {
-                    Console.WriteLine(f);
-                    listView1.Items.Add(f);
+                    Console.WriteLine(filePath);
+                    switch (System.IO.Path.GetExtension(filePath))
+                    {
+                        case ".txt":
+                            fileContents = File.ReadAllText(filePath);
+
+                            break;
+
+                        case ".doc":
+                        case ".docx":
+                        case ".xls":
+                        case ".xlsx":
+
+                            fileContents = GetDocumentPlainText(sDir, filePath);
+
+                            break;
+
+                    }
+                    switch (sender.Name)
+                    {
+                        case "allCases": // -가 있거나 없는 모든 주민번호
+                            if (Regex.IsMatch(fileContents, patternHyphen) || Regex.IsMatch(fileContents, patternNoHyphen))
+                            {
+                                IdStrings = new StringBuilder();
+                                foreach (var s in Regex.Matches(fileContents, patternHyphen))
+                                {
+                                    if (isId(s.ToString()))
+                                        IdStrings.Append(s.ToString() + " ");
+                                }
+                                foreach (var s in Regex.Matches(fileContents, patternNoHyphen))
+                                {
+                                    if (isId(s.ToString()))
+                                        IdStrings.Append(s.ToString() + " ");
+                                }
+                                if (!(IdStrings.Length == 0))
+                                {
+                                    ListViewItem listItem = new ListViewItem(filePath);
+                                    listItem.SubItems.Add(IdStrings.ToString());
+                                    resultListView.Items.Add(listItem);
+                                    //resultListView.Items.Add(f + "   주민번호 : " + IdStrings.ToString());
+                                }
+                            }
+                            break;
+                        case "noHyphen": //하이픈 없는것
+                            if (Regex.IsMatch(fileContents, patternNoHyphen))
+                            {
+                                IdStrings = new StringBuilder();
+                                foreach (var s in Regex.Matches(fileContents, patternNoHyphen))
+                                {
+                                    if (isId(s.ToString()))
+                                        IdStrings.Append(s.ToString() + " ");
+                                }
+                                if (!(IdStrings.Length == 0))
+                                {
+                                    ListViewItem listItem = new ListViewItem(filePath);
+                                    listItem.SubItems.Add(IdStrings.ToString());
+                                    resultListView.Items.Add(listItem);
+                                }
+                            }
+
+                            break;
+                        case "onlyHyphen": //하이픈 있는것만.
+                            if (Regex.IsMatch(fileContents, patternHyphen))
+                            {
+                                IdStrings = new StringBuilder();
+                                foreach (var s in Regex.Matches(fileContents, patternHyphen))
+                                {
+                                    if (isId(s.ToString()))
+                                        IdStrings.Append(s.ToString() + " ");
+                                }
+                                if (!(IdStrings.Length == 0))
+                                {
+                                    ListViewItem listItem = new ListViewItem(filePath);
+                                    listItem.SubItems.Add(IdStrings.ToString());
+                                    resultListView.Items.Add(listItem);
+                                }
+                            }
+
+                            break;
+                    }
                 }
+
+
+
+            }
+            catch
+            {
+            }
+
+            if ((File.GetAttributes(sDir) & FileAttributes.ReparsePoint) != FileAttributes.ReparsePoint)
+            {
                 foreach (string d in Directory.GetDirectories(sDir))
                 {
-                    FindFiles(d);
+                    FindFiles(d, sender);
                 }
             }
-            catch (System.Exception excpt)
-            {
-                Console.WriteLine(excpt.Message);
-            }
+
+
+
         }
 
         private void AddList(string f)
         {
-            this.listView1.Items.Add(f);
+            this.resultListView.Items.Add(f);
         }
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -86,7 +206,112 @@ namespace IDCollector
         {
             folderBrowserDialog1.ShowDialog();
             path = folderBrowserDialog1.SelectedPath;
-            label1.Text = path; 
+            folderLbl.Text = path;
+        }
+
+        //올바른 주민번호 판별 로직.
+        private bool isId(string _id)
+        {
+            int[] id = new int[13];
+            int idx = 0;
+            int key = 2;
+            int result = 0;
+            foreach (char s in _id)
+            {
+
+                if (!s.Equals('-'))
+                {
+                    id[idx] = Convert.ToInt32(s.ToString());
+                    idx++;
+                }
+
+            }
+            for (int i = 0; i < 12; i++)
+            {
+                if (key > 9)
+                {
+                    key = 2;
+                }
+                result = result + (id[i] * key);
+                key++;
+            }
+            if ((11 - (result % 11)) == id[12])
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string GetDocumentPlainText(string dir, string _fileName)
+        {
+            string plainText = string.Empty;
+            string ext = System.IO.Path.GetExtension(_fileName);
+
+            if (ext.Equals(".docx") || ext.Equals(".doc"))
+            {
+                //Create word document
+                Spire.Doc.Document document = new Spire.Doc.Document();
+
+                //load a document
+
+                try
+                {
+                    document.LoadFromFile(_fileName);
+                    plainText = document.GetText();
+                }
+                catch
+                {
+                }
+            }
+            else if (ext.Equals(".xlsx") || ext.Equals(".xls"))
+            {
+
+                Console.WriteLine(_fileName + "~~~ ");
+                //Create Excel workbook
+                Workbook workbook = new Workbook();
+
+                //load a workbook
+                workbook.LoadFromFile(_fileName);
+
+                for (int i = 0; i < workbook.Worksheets.Count; i++)
+                {
+                    string tmpfilename = "tempSheet" + i.ToString() + ".txt";
+                    Worksheet sheet = workbook.Worksheets[i];
+                    if (!sheet.IsEmpty)
+                    {
+                        if (System.IO.File.Exists(dir + tmpfilename))
+                        {
+                            System.IO.File.Delete(dir + tmpfilename);
+                        }
+                        sheet.SaveToFile(tmpfilename, ", ", Encoding.UTF8);
+                        plainText += "--[" + sheet.Name + "]--\r\n";
+                        plainText += System.IO.File.ReadAllText(tmpfilename);
+                        plainText += "\r\n";
+                    }
+                }
+            }
+
+            return plainText;
+        }
+
+        private void resultListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenExplorer(object sender, EventArgs e)
+        {
+
+            string path = System.IO.Path.GetDirectoryName(resultListView.SelectedItems[0].Text.ToString());
+            Console.WriteLine(path);
+            Process.Start(path);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
+
 }
+
